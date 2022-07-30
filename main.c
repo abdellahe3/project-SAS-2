f5fd7781200c09a2a23d36f822438ad0c738c024
Ayoub El Ayouk <ayoubelayouk@gmail.com>
2022-07-30 23:12:26 +0100

    multiply function added

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 int main()
 {
     printf("%d\n" , Sum(1 , 1));
+    printf("%d\n", multiply(12, 2));
     return 0;
 }
 
@@ -12,4 +13,8 @@ int Sum(int n1 , int n2 ){
     int sum = n1 + n2 ;
 
     return sum;
+}
+
+int multiply(int n1, int n2){
+    return n1 * n2;
 }
