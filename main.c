2d9d4ff33f3571c6a11f04ed12b6b512f21f6d6e
CS320 <lufy2024@gmail.com>
2022-07-30 21:37:24 +0100

    I have addded a function : Sum

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("%d\n" , Sum(1 , 1));
+    return 0;
+}
+
+int Sum(int n1 , int n2 ){
+    int sum = n1 + n2 ;
+
+    return sum;
+}
